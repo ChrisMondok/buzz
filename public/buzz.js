979a4c8f8e2d5077a42c12524d70035877e2e809
Chris Mondok <cmondok@marathondata.com>
2015-03-25 17:39:58 -0400

    Provide a nice animation

--- public/buzz.js
+++ public/buzz.js
@@ -12,5 +12,49 @@ function subscribeToBuzzes() {
 }
 
 function buzz() {
-    document.querySelector('audio').play();
+    var sound = getSound();
+    sound.play();
+    animateButton(sound.duration);
+}
+
+function getSound() {
+    return document.querySelector('audio');
+}
+
+window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame;
+
+function animateButton(duration) {
+    if( !( 'requestAnimationFrame' in window))
+        return;
+
+    var button = document.querySelector('button.buzz');
+
+    var remaining = Math.floor(1000 * duration);
+    var initial = remaining;
+    var lastTick = new Date();
+
+    tick();
+
+    function tick() {
+        var now = new Date();
+        var dt = now - lastTick;
+        lastTick = now;
+
+        remaining -= dt;
+
+        updateButton();
+
+        if(remaining > 0)
+            requestAnimationFrame(tick);
+    }
+
+    function updateButton() {
+        var magnitude = 0.35 * Math.max(Math.min(1, Math.pow((remaining/initial), 5)), 0);
+        var scale = 'scale('+ (1 + magnitude) + ')';
+
+        var rotate = 'rotate(' + magnitude * Math.sin(remaining/15) * 10 + 'deg)';
+
+        button.style.webkitTransform = [scale, rotate].join(' ');
+        button.style.transform = [scale, rotate].join(' ');
+    }
 }

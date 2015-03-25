8c2a3010bad6bf907a0f2065c96f861aaa9e1964
Chris Mondok <cmondok@marathondata.com>
2015-03-25 16:59:31 -0400

    Provide an error for browsers without EventSource

--- public/output.js
+++ public/output.js
@@ -1,10 +1,15 @@
-addEventListener('load', function() {
+if('EventSource' in window)
+    subscribeToBuzzes();
+else
+    alert('Your browser does not support EventSource. You will not hear anything.');
+
+function subscribeToBuzzes() {
     var es = new EventSource(location.origin+'/sse');
 
     es.addEventListener('buzz', function(buzzEvent) {
         buzz();
     });
-});
+}
 
 function buzz() {
     document.querySelector('audio').play();
